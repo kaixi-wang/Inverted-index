0afb7b25de7b1701ea94993e947c782c92bf2fdb
Kacey <kaixiwan@usc.edu>
2020-03-01 06:45:49 -0800

    Create Mapper.py

---
+++ Mapper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Mapper reads data from STDIN, split it into words and output a list of linse mapping words to their counts to STDOUT
+# Note: mapper will not compute an intermediate sum of a word's ocurrances
+# Instead, it will output <word> 1 tuples immediately, and subsequent Reduce step do the final sum count
+
+# Make sure file has execution permission ( chmod +x  ./mapper.py)
+
+data={}
+for contents in stdin:
+    content=contents.split('\t')
+    docID=content[0]
+    txts=content[1:]
+    for txt in txts:
+        words = re.findall(r'\w+', content)
+        for word in words:
+            print '%s/t%s' % (word, 1)
